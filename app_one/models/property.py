eedd227bd7988b4f7d436d27cc1e09fea3672d8f
haythamgamal6 <haythamgamal6@gmail.com>
2024-10-30 17:58:38 +0300

    # open view from another view

--- app_one/models/property.py
+++ app_one/models/property.py
@@ -125,6 +125,15 @@ class Property(models.Model):
             else:
                 rec.next_time = False
 
+    # open view from another view
+    def action_open_related_owner(self):
+        action = self.env['ir.actions.actions']._for_xml_id('app_one.owner_action')
+        view_id = self.env.ref('app_one.owner_form_view').id
+        action['res_id'] = self.owner_id.id
+        action['views'] = [(view_id, 'form')]
+
+        return action
+
 
 class PropertyLine(models.Model):
     _name = "property.line"
