5fcc168faba4f86c8e7b40126da6a7ebdfad9755
haythamgamal6 <haythamgamal6@gmail.com>
2024-11-06 12:50:52 +0200

    add api update and delete

--- app_one/controllers/property_api.py
+++ app_one/controllers/property_api.py
@@ -1,4 +1,5 @@
 import json
+from logging import exception
 
 from docutils.nodes import status
 from odoo import http
@@ -47,11 +48,9 @@ class PropertyApi(http.Controller):
                 "message": str(error),
             }, status=400)
 
-
-
     class PropertyController(http.Controller):
 
-        @route("/v1/property/<int:property_id>", methods=["PUT"], type="http", auth="none", csrf=False)
+        @http.route("/v1/property/<int:property_id>", methods=["PUT"], type="http", auth="none", csrf=False)
         def update_property(self, property_id):
             property_record = request.env['property'].sudo().search([("id", "=", property_id)])
 
@@ -71,3 +70,39 @@ class PropertyApi(http.Controller):
 
             except json.JSONDecodeError:
                 return request.make_json_response({"message": "Invalid JSON data"}, 400)
+
+        @http.route("/v1/property/<int:property_id>", methods=["GET"], type="http", auth="none", csrf=False)
+        def get_property(self, property_id):
+            try:
+                property_record = request.env['property'].sudo().search([("id", "=", property_id)])
+                if property_record:
+                    return request.make_json_response(
+                        {
+                            "name": property_record.name,
+                            "description": property_record.description,
+                            "postcode": property_record.postcode,
+                            "state": property_record.state,
+                        }, status=200)
+
+                else:
+                    return request.make_json_response({"message": "property_id not exists"}, status=400)
+
+            except Exception as error:
+                return request.make_json_response({"message": error}, status=400)
+
+        @http.route("/v1/property/<int:property_id>", methods=["DELETE"], type="http", auth="none", csrf=False)
+        def delete_property(self, property_id):
+            try:
+                property_record = request.env['property'].sudo().search([("id", "=", property_id)])
+                if property_record:
+                    property_record.unlink()
+                    return request.make_json_response(
+                        {
+                            "message": "property has been removed successfully"
+                        }, status=200)
+
+                else:
+                    return request.make_json_response({"message": "property_id not exists"}, status=400)
+
+            except Exception as error:
+                return request.make_json_response({"message": error}, status=400)
