9c84435402cb19fc654355e7391bd19942e339d8
haythamgamal <haythamgamal6@gmail.com>
2025-01-15 21:46:58 +0200

    pos test js and not work

--- test_pos_custom/models/__init__.py
+++ test_pos_custom/models/__init__.py
@@ -2,3 +2,4 @@
 
 from . import models
 from . import pos_config
+from . import res_partner
--- test_pos_custom/static/src/js/add_payment_method.js
+++ test_pos_custom/static/src/js/add_payment_method.js
@@ -1,17 +1,27 @@
-odoo.define('test_pos_custom.add_payment_method', function (require) {
-    "use strict";
-    var screens = require('point_of_sale.screens');
-    screens.PaymentScreenWidget.include({
-show : function(){
-        this._super();
-     var default_method = this.pos.config.default_payment_method;
-     if(default_method){
-      this.clickPaymentMethod(default_method(0));
-     }
-     else{
-     }
-     }
+odoo.define("test_pos_custom.add_payment_method", function (require) {
+  "use strict";
 
+  var screens = require("point_of_sale.screens");
+  var models = require("point_of_sale.models");
+  models.load_fields("res.partner", ["code"]);
 
+  screens.PaymentScreenWidget.include({
+    show: function () {
+      this._super(); // استدعاء الدالة الأصلية
 
+      var order = this.pos.get_order().get_client(); // الحصول على الطلب الحالي
+      var default_method = this.pos.config.default_payment_method;
+
+      if (default_method) {
+        this.clickPaymentMethod(default_method[0]); // اختيار طريقة الدفع الافتراضية
+      }
+
+      if (order) {
+        console.log("Client Information:", order);
+        console.log("Client Information:", order.code);
+      } else {
+        console.log("No client set for the current order.");
+      }
+    },
+  });
 });
