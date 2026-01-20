0041a51574ca46eff03cd19b6b6310f2bbab750c
haytham gamal <haythamgamal6@gmail.com>
2026-01-20 17:00:04 +0200

    feat: add task completion toggle with proper parent-child communication

--- addons_version_18/owl_modules/Todo App/app.js
+++ addons_version_18/owl_modules/Todo App/app.js
@@ -1,33 +1,36 @@
 const { Component, mount, useState, xml } = owl;
 
 class Task extends Component {
-  static template = xml`              <li t-att-style="'background-color: ' + props.task.color" 
-                  class="d-flex justify-content-between align-items-center p-3 border mb-3 rounded-3">
+  static template = xml`
+    <li t-att-style="'background-color: ' + props.task.color" 
+        class="d-flex justify-content-between align-items-center p-3 border mb-3 rounded-3">
 
-                <div class="form-check form-switch fs-5">
-                  <input class="form-check-input"
-                         type="checkbox"
-                         t-att-id="props.task.id"/>
-                  <label class="form-check-label" 
-                         t-att-for="props.task.id">
-                    <t t-esc="props.task.name"/>
-                  </label>
-                </div>
+      <div class="form-check form-switch fs-5">
+        <input class="form-check-input"
+               type="checkbox" 
+               t-on-click="() => props.onToggle(props.task.id)"
+               t-att-checked="props.task.isCompleted"
+               t-att-id="props.task.id"/>
+        <label class="form-check-label" 
+               t-att-for="props.task.id"
+               t-attf-class="{{props.task.isCompleted ? 'text-decoration-line-through' : ''}}">
+          <t t-esc="props.task.name"/>
+        </label>
+      </div>
 
-                <div>
-                  <button class="btn btn-primary me-2">
-                    <i class="bi bi-pencil"></i>
-                  </button>
+      <div>
+        <button class="btn btn-primary me-2">
+          <i class="bi bi-pencil"></i>
+        </button>
 
-                  <button class="btn btn-danger">
-                    <i class="bi bi-trash"></i>
-                  </button>
-                </div>
+        <button class="btn btn-danger">
+          <i class="bi bi-trash"></i>
+        </button>
+      </div>
+    </li>
+  `;
 
-              </li>
-`;
-
-  static props = ["task"];
+  static props = ["task", "onToggle"];
 }
 
 class Root extends Component {
@@ -35,7 +38,6 @@ class Root extends Component {
     <div class="container mt-5">
       <div class="row my-5 py-5">
         <div class="col-lg-6 offset-lg-3">
-
           <h1 class="text-center mb-4 fw-bold main_title">Todo List OWL</h1>
           <p class="text-center h5 text-muted mb-5">OWL Tutorial for Beginners</p>
 
@@ -55,17 +57,19 @@ class Root extends Component {
               ADD TASK
             </button>
           </div>
-               <ul class="d-flex flex-column mt-5 p-0">
+
+          <ul class="d-flex flex-column mt-5 p-0">
             <t t-foreach="tasks" t-as="task" t-key="task.id">
-          <Task  task="task"/>
+              <Task task="task" onToggle.bind="toggleTask"/>
             </t>
           </ul>
-
         </div>
       </div>
     </div>
   `;
+
   static components = { Task };
+
   setup() {
     this.state = useState({
       name: "",
@@ -93,6 +97,13 @@ class Root extends Component {
 
     this.state.name = "";
     this.state.color = "#00C897";
+  }
+
+  toggleTask(taskId) {
+    const task = this.tasks.find(t => t.id === taskId);
+    if (task) {
+      task.isCompleted = !task.isCompleted;
+    }
   }
 }
 
