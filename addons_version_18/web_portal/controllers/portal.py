97ca81c3d7922571c59a58c257ff66e162194e0a
haytham gamal <haythamgamal6@gmail.com>
2026-02-04 23:00:39 +0200

    feat(portal/properties): add properties portal with list, detail, and breadcrumbs

--- addons_version_18/web_portal/controllers/portal.py
+++ addons_version_18/web_portal/controllers/portal.py
@@ -5,6 +5,7 @@ from odoo.addons.portal.controllers.portal import CustomerPortal, pager as porta
 
 
 class PropertyPortal(CustomerPortal):
+    _items_per_page = 10
 
     def _prepare_home_portal_values(self, counters):
         values = super()._prepare_home_portal_values(counters)
@@ -24,12 +25,31 @@ class PropertyPortal(CustomerPortal):
         domain = self._get_property_domain()
 
         property_count = Property.search_count(domain)
+        page_start = 0
+        page_end = 0
+        if property_count:
+            page_start = ((page - 1) * self._items_per_page) + 1
+            page_end = min(page * self._items_per_page, property_count)
         pager = portal_pager(
             url='/my/properties',
             total=property_count,
             page=page,
             step=self._items_per_page
         )
+        page_count = pager.get('page_count', 1)
+        page_links = [
+            {
+                'num': p,
+                'url': '/my/properties' if p == 1 else '/my/properties/page/%s' % p
+            }
+            for p in range(1, page_count + 1)
+        ]
+        prev_url = None
+        next_url = None
+        if page > 1:
+            prev_url = '/my/properties' if page - 1 == 1 else '/my/properties/page/%s' % (page - 1)
+        if page < page_count:
+            next_url = '/my/properties/page/%s' % (page + 1)
 
         properties = Property.search(
             domain,
@@ -42,6 +62,12 @@ class PropertyPortal(CustomerPortal):
             'page_name': 'properties',
             'properties': properties,
             'pager': pager,
+            'property_count': property_count,
+            'page_start': page_start,
+            'page_end': page_end,
+            'page_links': page_links,
+            'prev_url': prev_url,
+            'next_url': next_url,
             'default_url': '/my/properties',
         })
         return request.render('web_portal.portal_my_properties', values)
